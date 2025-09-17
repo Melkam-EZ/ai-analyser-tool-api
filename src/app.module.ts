5b4c5792aaa6d9d7be427192457307fabf6e8b84
Melkam Eziokwu <melkamev@gmail.com>
2025-09-16 20:08:09 -0500

    feat: added aws bedrock module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BedrockModule } from './common/bedrock';
 
 @Module({
-  imports: [],
+	imports: [BedrockModule],
   controllers: [AppController],
   providers: [AppService],
 })
